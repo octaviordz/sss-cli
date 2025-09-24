1b410174298c49ff3ed9f490f2294ac95555ffa5
Octavio Rodríguez-Castañeda <octavio.rdz@outlook.com>
2025-09-24 04:26:33 -0600

    Use `clap` for argument parsing; improve input handling  Replaced `std::env::args` with the `clap` crate in `echo.rs` , adding support for the `-n` flag to  suppress newlines. Introduced an `argparse` function for argument handling.  In `split.rs`, added buffering logic to read input files into  memory using `std::io::{Read, Cursor}` for trace, debug use.. Commented out unused debugging  code.  Updated `.gitignore` to exclude Visual Studio files (`.vs/`).

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,7 +1,28 @@
 #![deny(warnings)]
+extern crate clap;
 
-use std::env::args;
+use clap::{App, Arg, ArgMatches};
+
+fn argparse<'a>() -> ArgMatches<'a> {
+    App::new("echo")
+        .about("echo executable for testing")
+        .arg(Arg::with_name("n")
+                 .short("n")
+                 .help("do not append newline")
+                 .takes_value(false)
+                 .required(false))
+        .arg(Arg::with_name("arg")
+                 .help("arg to echo"))
+        .get_matches()
+}
 
 fn main() {
-    println!("{}", args().skip(1).collect::<Vec<_>>().join(" "));
+    let matches = argparse();
+    let itr = matches.values_of_lossy("arg").unwrap().into_iter();
+    let echo_msg = itr.collect::<Vec<_>>().join(" ");
+    if matches.is_present("n") {
+        print!("{}", echo_msg);
+    }else {
+        println!("{}", echo_msg);
+    }
 }
