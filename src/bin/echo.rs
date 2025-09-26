414f596da32c8bb7f39687263046c3ef1dd5748e
Octavio Rodríguez-Castañeda <octavio.rdz@outlook.com>
2025-09-26 01:52:54 -0600

    Improve argument parsing and code readability
    Update the logic in `main` to handle cases where "arg" values may be `None`.

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -7,22 +7,28 @@ fn argparse<'a>() -> ArgMatches<'a> {
     App::new("echo")
         .about("echo executable for testing")
         .arg(Arg::with_name("n")
-                 .short("n")
-                 .help("do not append newline")
-                 .takes_value(false)
-                 .required(false))
+                .short("n")
+                .help("do not append newline")
+                .takes_value(false)
+                .required(false),
+        )
         .arg(Arg::with_name("arg")
-                 .help("arg to echo"))
+            .help("arg to echo"))
         .get_matches()
 }
 
 fn main() {
     let matches = argparse();
-    let itr = matches.values_of_lossy("arg").unwrap().into_iter();
-    let echo_msg = itr.collect::<Vec<_>>().join(" ");
+    let echo_msg = match matches.values_of_lossy("arg") {
+        None => String::from(""),
+        Some(x) => {
+            let v = x.into_iter().collect::<Vec<_>>();
+            v.join(" ")
+        }
+    };
     if matches.is_present("n") {
         print!("{}", echo_msg);
-    }else {
+    } else {
         println!("{}", echo_msg);
     }
 }
